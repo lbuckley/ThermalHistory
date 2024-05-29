bb71589431f4173b28e9cb89bb466f914857eea9
Lauren Buckley <lbbuckley@gmail.com>
2024-05-28 17:01:27 -0700

    update and split aphid analysis

--- AphidTPCanalysis.R
+++ AphidTPCanalysis.R
@@ -12,121 +12,7 @@ library(TrenchR)
 setwd("/Users/laurenbuckley/ThermalHistory")
 #setwd("/Users/lbuckley/ThermalHistory") #laptop
 
-#Load data
-#Ma et al. 2021. Are extreme high temperatures at low or high latitudes more likely to inhibit the population growth of a globally distributed aphid?
-#https://doi.org/10.1016/j.jtherbio.2021.102936
-
-#2.1 Temp regimes Fig A2
-#During the day, temperature started to increase at 08:00 h, reached and stayed at a high level (35, 37 or 39 ◦C) from 12:00 to 13:00 h, and then decreased to 22 ◦C by 16:00 h. We kept temperature constant at 22 ◦ C for the rest of the day. Importantly, we set both the daily mean (25.2 ◦C) and minimum (22 ◦C) temperatures as fixed
-
-#construct temperatures
-temps= read.csv("./data/temps_Maetal2021JTB.csv")
-
-#add other hours
-tadd<- cbind(time=c(1:8,16:24),temp=22)
-tadd<- rbind( cbind(rep(39, 17),tadd), cbind(rep(37, 17),tadd), cbind(rep(35, 17),tadd) ) 
-colnames(tadd)[1]<- "treatment"
-temps<- as.data.frame(rbind(temps, tadd))
-temps<- temps[order(temps[,1], temps[,2]),]
-
-days<- rep(1:20, nrow(temps))
-days<- days[order(days)]
-temps.all<- cbind(temps, days)
-temps.all$dt<- temps.all$days+temps.all$time/24
-
-#plot temps
-ggplot(data=temps.all, aes(x=dt, y=temp, color=factor(treatment)))+geom_line()
-
-#--------------
-#2.4 constant temp TPCs
-#Params from Table 1, fitting data in appendix
-
-#nymphal survival
-Sur= function(T, a=334.1, b= -0.29) 1-T/(a-b*T^2)
-
-#Adult longevity (days)
-Long= function(T, a=29.5, b= -0.81) a + b*T
-
-#Lifetime fecundity (nymphs / adult)
-Fecun= function(T, a= -0.3, b= 11.1, c= -50.9) a*T^2 + b*T +c
-
-#Reproductive rate (nymphs/adult/day)
-Rr= function(T, a=0.263, b=3.7, T0 =30.1) exp(a*T)-exp(a*T0-(T0-T)/b)
-
-#Developmental rate (1/days)
-Dr= function(T, a=0.149, b=6.6, T0 =33.1) exp(a*T)-exp(a*T0-(T0-T)/b)
-
-#Intrinsic rate of increase
-Rm= function(T, a=0.177, b=5.6, T0 =30.2) exp(a*T)-exp(a*T0-(T0-T)/b)    
-
-#Plot
-p1= cbind(t=1:40, p=Sur(1:40), comp="Sur")
-p2= cbind(t=1:40, p=Long(1:40), comp="Long")
-p3= cbind(t=1:40, p=Fecun(1:40), comp="Fecun")
-p4= cbind(t=1:40, p=Rr(1:40), comp="Rr")
-p5= cbind(t=1:40, p=Dr(1:40), comp="Dr")
-p6= cbind(t=1:40, p=Rm(1:40), comp="Rm")
-pall<- as.data.frame(rbind(p1, p2, p3, p4, p5, p6))
-pall$t<- as.numeric(pall$t); pall$p<- as.numeric(pall$p)
-
-fig1<- ggplot(data=pall, aes(x=t, y =p))+geom_line()+
-  facet_wrap(.~comp, scales="free_y")+
-  scale_y_continuous(limits = c(0, NA))
-
-#need to fix fecundity
-fec= cbind(temp=c(10,15,20,25,18,22,25,27.5,30),
-           fec=c(30,55,60,41,39.5,35.2,42.4,45,1.6))
-fec= as.data.frame(fec)
-fec$temp2<- fec$temp^2
-
-ggplot(data=fec, aes(x=temp, y =fec))+geom_point()
-lm1<- lm(fec~temp + temp2 , data=fec)
-#c= -50.9, a=11.1, b= -0.3
-
-#--------------------------
-#estimate performance using constant TPC
-
-#nymphal survival
-temps.all$sur<- Sur(temps.all$temp)
-
-#Adult longevity (days)
-temps.all$long<- Long(temps.all$temp)
-
-#Lifetime fecundity (nymphs / adult)
-temps.all$fecun<- Fecun(temps.all$temp)
-
-#Reproductive rate (nymphs/adult/day)
-temps.all$rr<- Rr(temps.all$temp)
-
-#Developmental rate (1/days)
-temps.all$dr<- Dr(temps.all$temp)
-
-#Intrinsic rate of increase
-temps.all$r<- Rm(temps.all$temp)
-
-#set values less than 0 to 0
-temps.all$rr[temps.all$rr<0] <- 0
-temps.all$dr[temps.all$dr<0] <- 0
-temps.all$r[temps.all$r<0] <- 0
-
-#plot constant rates
-#sum performance
-perf= temps.all %>%
-  group_by(treatment) %>%
-  summarise(Sur=mean(sur), Long=mean(long), Fecun=mean(fecun), Rr=mean(rr), Dr=mean(dr), Rm=mean(r) )
-            
-#to long format
-perf<- perf %>%
-  gather("comp", "p", c(2:ncol(perf)) )
-perf$t<- 25 #mean temp
-
-#plot
-fig1+  geom_point(data=perf, aes(color=factor(treatment)))
-
-#--------------------------
-#adapt Kingsolver Woods model
-
-tfun<- function(Time) temps[Time]
+#Kingsolver and Woods model
 
 RsigCG = function(Time, State, Pars) {
   with(as.list(c(State, Pars)), {
@@ -146,78 +32,85 @@ RsigCG = function(Time, State, Pars) {
 pars <- c(tauR = 3,  tauP = 20, Rm2 = 10, Pm=10, Tc = 35, Rc = 5, aIng = 0.02, bIng = 0.8, C = 0.8, k = 0.5)
 yini <- c(R = 0, P = 0, Mass = 0.1)
 
-#loop temperatures
-treats<- c(35,37,39)
-
-for(treat.k in 1:3){
-
-temps<- c(temps.all[temps.all$treatment==treats[treat.k],"temp"])
-times<- 1:length(temps)  #c(temps.all[temps.all$treatment==35,"dt"])
-
-out2 <- ode(func = RsigCG, y = yini, parms = pars, times = times)
-
-#convert to dataframe
-out2.df <- data.frame(out2)
-colnames(out2.df)[5:7]<- c("T","I","G") 
-#summary(out2.df)
-out2.df$treatment<- treats[treat.k]
-
-if(treat.k==1) pout<- out2.df
-if(treat.k>1) pout<- rbind(pout, out2.df)
-
-} #end loop treatments
-
-#---------
-#plot Kingsolver model
-
-#to long format
-pout.l<- melt(pout, id.vars = c("time","treatment","T"), variable.name = "trait")
-
-ggplot(data=pout.l, aes(x=time, y =value, color=factor(treatment)))+geom_line()+
-  facet_wrap(.~trait, scale="free_y")
-
-#plot Kingsolver model
-#sum performance
-perf= pout.l %>%
-  group_by(treatment) %>%
-  summarise(value= mean(value))
-
-#=================================
-#Wang and Ma. 2023. Can laboratory‐reared aphid populations reflect the thermal performance of field populations in studies on pest science and climate change biology?  JPS
-#https://doi.org/10.1007/s10340-022-01565-6 
-
-#check data formatting
-setwd("/Users/laurenbuckley/Google Drive/My Drive/Buckley/Work/ThermalHistory/data/aphids/")
-#setwd("/Volumes/GoogleDrive/My Drive/Buckley/Work/ThermalHistory/data/aphids/")
-
-adat1<- read.csv("WangMa2023_temp22mean.csv")
-
-#mean metric
-adat1.mean= adat1 %>%
-  group_by(Tmean, Tvar, metric, population) %>%
-  summarise(value= mean(value))
-
-ggplot(data=adat1.mean, aes(x=Tvar, y =value, color=population))+geom_point()+
-  facet_grid(Tmean~metric, scale="free_y")
-
-#======
-#Ma et al. 2015. Daily temperature extremes play an important role in predicting thermal effects. The Journal of Experimental Biology 218 (14), 2289-2296
-#https://doi.org/10.1242/jeb.122127, no data in paper
-
 #=====
 #Zhao et al. 2014. Night warming on hot days produces novel impacts on development, survival and reproduction in a small arthropod
 #Dryad data: http://doi.org/10.5061/dryad.q2070 
 
-adat2.dt<- read.csv("Zhaoetal2014_devtime.csv")
-adat2.p<- read.csv("Zhaoetal2014_AdPerf.csv")
-adat2.lt<- read.csv("Zhaoetal2014_LifeTable.csv")
-adat2.sur<- read.csv("Zhaoetal2014_SurvNymph.csv")
+setwd("/Users/laurenbuckley/Google Drive/My Drive/Buckley/Work/ThermalHistory/data/aphids/")
+#setwd("/Volumes/GoogleDrive/My Drive/Buckley/Work/ThermalHistory/data/aphids/")
 
+adat2.dt<- read.csv("Zhaoetal2014/Zhaoetal2014_devtime.csv")
+adat2.p<- read.csv("Zhaoetal2014/Zhaoetal2014_AdPerf.csv")
+adat2.lt<- read.csv("Zhaoetal2014/Zhaoetal2014_LifeTable.csv")
+adat2.sur<- read.csv("Zhaoetal2014/Zhaoetal2014_SurvNymph.csv")
+
+#----
+#construct temperatures
+#currently sawtooth, make sine wave?
+
+#day: increased from 27 °C to peak at 35 °C (at 09.00–15.00 h) and then decreased to 27 °C (at 15.00–21.00 h)
+#temperatures in the six chambers dropped from 27 °C to different minima (13, 16, 19, 21, 23 and 25 °C) at 21.00–03.00 h and then rose to 27 °C (at 03.00–09.00 h). 
+
+tmin= c(13, 16, 19, 21, 23, 25)
+
+t.hrs= c(22:24,1:8)
+tmin.fun= function(tmin) c( 27-1*(27-tmin)/6, 27-2*(27-tmin)/6, 27-3*(27-tmin)/6, 27-4*(27-tmin)/6, 27-5*(27-tmin)/6, tmin, 27-5*(27-tmin)/6, 27-4*(27-tmin)/6, 27-3*(27-tmin)/6, 27-2*(27-tmin)/6, 27-1*(27-tmin)/6)
+
+temps<- matrix(NA, nrow=length(tmin), ncol=24)
+for(t.k in 1:length(tmin)){
+  ts= tmin.fun(tmin[t.k])
+  temps[t.k, 1:8]= ts[match(1:8, t.hrs)]
+  temps[t.k, 9:21]= c(27, 27+1*8/6, 27+2*8/6, 27+3*8/6, 27+4*8/6, 27+5*8/6, 35, 27+5*8/6, 27+4*8/6, 27+3*8/6, 27+2*8/6, 27+1*8/6, 27)
+  temps[t.k, 22:24]= ts[match(22:24, t.hrs)]
+}
+
+temps= as.data.frame(cbind(tmin, temps))
+colnames(temps)[2:25]<- 1:24
+
+#to long format
+temps.l<- melt(temps, id.vars = c("tmin"), variable.name = "hr")
+
+#plot
+ggplot(data=temps.l, aes(x=hr, y =value, color=tmin, group=tmin))+geom_line()
+
+#-----
+#Constant rate TPCs
+#development rate
+#chinese clones
+dr= function(T, Tmax=34.09, a=0.13, b=4.43, c=7.65) {
+  dr=exp(a*T)-exp(b-(Tmax-T)/c)
+  dr[dr<0]<- 0
+  return(dr)
+}
+  
+#European clones
+dr.e= function(T, Tmax=32.91, a=0.13, b=4.28, c=7.65) exp(a*T)-exp(b-(Tmax-T)/c)
+
+#survival
+sur= function(T, a=345.51, b=0.35) {
+  sur=1-T/(a-b*T^2)
+  sur[T>30]<- 0
+  return(sur)
+}
+
+long= function(T, a=32.73, b= -0.91) 
+{
+  lon= a + b*T
+  lon[lon<0]<- 0
+  return(lon)
+}
+  
+fec= function(T, a= -69.1, b=12.49, c= -0.34){
+  fec=a +b*T +c*T^2
+  fec[fec<0]<- 0
+  return(fec)
+}
+  
 #----
 #dev time
 colnames(adat2.dt)[c(1,3,5,7,9)]<-"NTmin"
 colnames(adat2.dt)[c(2,4,6,8,10)]<-"dt"
-adat2.dt.l<- rbind( cbind(adat2.dt[,1:2], stage="1st"), cbind(adat2.dt[,1:2], stage="2nd"), cbind(adat2.dt[,1:2], stage="3rd"), cbind(adat2.dt[,1:2], stage="4th"),cbind(adat2.dt[,1:2], stage="Nymph") )
+adat2.dt.l<- rbind( cbind(adat2.dt[,1:2], stage="1st"), cbind(adat2.dt[,3:4], stage="2nd"), cbind(adat2.dt[,5:6], stage="3rd"), cbind(adat2.dt[,7:8], stage="4th"),cbind(adat2.dt[,9:10], stage="Nymph") )
 
 #mean metric
 adat2.dt.m= adat2.dt.l %>%
@@ -225,7 +118,9 @@ adat2.dt.m= adat2.dt.l %>%
   summarise(dt= mean(dt))
 
 ggplot(data=adat2.dt.m, aes(x=stage, y =dt, color=factor(NTmin), group=factor(NTmin)))+geom_line()
-#fix straight lines
+
+#as tpc
+ggplot(data=adat2.dt.m, aes(x=NTmin, y =1/dt, color=stage, group=stage))+geom_line()
 
 #----
 #performance
@@ -248,12 +143,6 @@ ggplot(data=adat2.lt, aes(x=day, y =M.x., color=factor(NTmin), group=factor(NTmi
 #----
 #survival
 ggplot(data=adat2.sur, aes(x=NTmin, y =Days, color=factor(NTmin), group=factor(NTmin) ))+geom_point()+geom_smooth()+facet_wrap(.~Status)
-
-#=====
-#Zhao et al. The importance of timing of heat events for predicting the dynamics of aphid pest populations. Pest management science, 2019
-#https://doi.org/10.1002/ps.5344
-#survival and productivity
-#no data online
 
 #=====
 #Ma CS, Wang L, Zhang W, Rudolf V 2018. Resolving biological impacts of multiple heat waves: interaction of hot and recovery days. Oikos 127:622–33
@@ -362,36 +251,26 @@ ggplot(data=adat3.rep.m, aes(x=Treatment, y =rep, color=Hot_Cold))+geom_point()
 
 perf.fun<- function(ts){
   #nymphal survival
-  sur1<- mean(as.numeric(Sur(ts)))
+  sur1<- mean(as.numeric(sur(ts)))
   
   #Adult longevity (days)
-  long1<- mean(as.numeric(Long(ts)))
+  long1<- mean(as.numeric(long(ts)))
   
   #Lifetime fecundity (nymphs / adult)
-  fecun1<- mean(as.numeric(Fecun(ts)))
-  
-  #Reproductive rate (nymphs/adult/day)
-  rr1<- as.numeric(Rr(ts))
-  rr1[rr1<0] <- 0
-  rr1<- mean(rr1)
+  fecun1<- mean(as.numeric(fec(ts)))
   
   #Developmental rate (1/days)
-  dr1<- as.numeric(Dr(ts))
+  dr1<- as.numeric(dr(ts))
   dr1[dr1<0] <- 0
   dr1<- mean(dr1)
   
-  #Intrinsic rate of increase
-  r1<- as.numeric(Rm(ts))
-  r1[r1<0] <- 0
-  r1<- mean(r1)
-  
-  return( c(sur1, long1, fecun1, rr1, dr1, r1) )
+  return( c(sur1, long1, fecun1, dr1) )
 }
 
 #estimate performance
 tz= t(apply(temps[,4:720], MARGIN=1, FUN=perf.fun))
 tp= cbind(temps[,1:3], tz)
-colnames(tp)[4:9]<- c("sur", "Longevity", "Fecundity", "rr", "dr", "r")
+colnames(tp)[4:7]<- c("sur", "Longevity", "Fecundity", "dr")
 tp$NymphDur<- 1/tp$dr
 
 #to long format
@@ -463,12 +342,125 @@ perf$metric<- as.character(perf$metric)
 perf$metric[which(perf$metric=="G")]<-"Fecundity"
 perf$metric<- as.factor(perf$metric)
 
+#plot Kingsolver and Woods estimate
+ggplot(perf, aes(x=ContinueHotday, y =value, color=factor(ContinueNormalDay)))+geom_line()+
+  facet_grid(metric~H_C, scale="free_y")
+#very small differences, scale?
+
 #add to plot
 fig.trait3<- fig.trait2+  geom_line(data=perf[which(perf$metric=="Fecundity"),], linetype="dotted", linewidth=2)
 
-#=========================================
-#Zhang W, Chang XQ, Hoffmann AA, Zhang S and Ma CS, Impact of hot events at different developmental stages of a moth: the closer to adult stage, the less reproductive output. Sci Rep 5: 1–9 (2015).
-#Plutella xylostella heat wave experiment
-#No data online
+#=====
+#Wang, XJ., Ma, CS. Can laboratory-reared aphid populations reflect the thermal performance of field populations in studies on pest science and climate change biology?. J Pest Sci 96, 509–522 (2023). https://doi.org/10.1007/s10340-022-01565-6
 
+#read data
+setwd("/Users/laurenbuckley/Google Drive/My Drive/Buckley/Work/ThermalHistory/data/aphids/WangMa2023/")
+#setwd("/Volumes/GoogleDrive/My Drive/Buckley/Work/ThermalHistory/data/aphids/WangMa2023/")
+
+adat4.var<- read.csv("WangMa2023_temp22mean_diffvar.csv")
+adat4.mean<- read.csv("WangMa2023_diffmeans.csv")
+
+#----------
+#set up temperatures
+#mean: 22 °C; ±0 °C,±5 °C,±9 °C,±13 °C as fluctuating ranges, and designed four #3 high constant (28 °C, 30 °C, 32 °C) and fluctuating regimes (28 ± 5 °C, 30±5 °C, 32±5 °C)
+
+temps= matrix(NA, nrow= 10, ncol=24)
+temps[1,]<- 22
+temps[2,]<- diurnal_temp_variation_sine(T_max = 27, T_min = 17, 1:24)
+temps[3,]<- diurnal_temp_variation_sine(T_max = 31, T_min = 13, 1:24)
+temps[4,]<- diurnal_temp_variation_sine(T_max = 35, T_min = 9, 1:24)
+temps[5,]<- 28
+temps[6,]<- 30
+temps[7,]<- 32
+#28 ± 5
+temps[8,]<- diurnal_temp_variation_sine(T_max = 33, T_min = 23, 1:24)
+#30±5
+temps[9,]<- diurnal_temp_variation_sine(T_max = 35, T_min = 25, 1:24)
+#32±5
+temps[10,]<- diurnal_temp_variation_sine(T_max = 37, T_min = 27, 1:24)
+
+temps<- as.data.frame(temps)
+colnames(temps)= 1:24
+
+##expand to 50 days
+#temps<- as.data.frame(rep(temps, each = 50))
+
+Tmean= c(22, 22, 22, 22, 28, 30, 32, 28, 30, 32)
+Tvar= c(0, 5, 9, 13, 0, 0, 0, 28, 30, 32)
+treat= c("mild means", "mild means", "mild means", "mild means", "high means", "high means", "high means", "high means", "high means", "high means")
+temps= as.data.frame(cbind(Tmean, Tvar, treat, temps))
+temps$Tmean_var= paste(temps$Tmean, temps$Tvar, sep="_")
+
+#to long format
+temps.l<- melt(temps, id.vars = c("Tmean", "Tvar", "treat", "Tmean_var"), variable.name = "time")
+temps.l$time= as.numeric(temps.l$time)
+temps.l$value= as.numeric(temps.l$value)
+
+ggplot(data=temps.l, aes(x=time, y =value, color=Tvar, group=Tmean_var))+geom_line()+
+  facet_grid(.~treat, scale="free_y")
+
+#----------
+#Tmean of 22, difference variance
+#sum performance
+adat4.var.m= adat4.var %>%
+  group_by(Tmean, Tvar, population, metric) %>%
+  summarise(value= mean(value))
+
+ggplot(data=adat4.var.m, aes(x=Tvar, y =value, color=population))+geom_point()+ geom_line()+
+  facet_grid(metric~Tmean, scale="free_y")
+
+#Different means
+#sum performance
+adat4.mean.m= adat4.mean %>%
+  group_by(Tmean, Tvar, population, metric) %>%
+  summarise(value= mean(value))
+
+ggplot(data=adat4.mean.m, aes(x=Tvar, y =value, color=population))+geom_point()+ geom_line()+
+  facet_grid(metric~Tmean, scale="free_y")
+
+#--------------------------
+#estimate performance using constant TPC
+
+perf.fun<- function(ts){
+  #nymphal survival
+  sur1<- mean(as.numeric(sur(ts)))
+  
+  #Adult longevity (days)
+  long1<- mean(as.numeric(long(ts)))
+  
+  #Lifetime fecundity (nymphs / adult)
+  fecun1<- mean(as.numeric(fec(ts)))
+  
+  #Developmental rate (1/days)
+  dr1<- as.numeric(dr(ts))
+  dr1[dr1<0] <- 0
+  dr1<- mean(dr1)
+  
+  return( c(sur1, long1, fecun1, dr1) )
+}
+
+#estimate performance
+tz= t(apply(temps[,4:(ncol(temps)-1)], MARGIN=1, FUN=perf.fun))
+tp= cbind(temps[,1:3], tz)
+colnames(tp)[4:7]<- c("sur", "Longevity", "Fecundity", "dr")
+tp$NymphDur<- 1/tp$dr
+
+#to long format
+tp.l<- melt(tp, id.vars = c("Tmean","Tvar","treat"), variable.name = "metric")
+
+#plot estimates
+ggplot(data=tp.l, aes(x=Tmean, y=value, color=Tvar)) +geom_point()+
+  geom_smooth(method='lm') +geom_point()+
+  facet_grid(metric~treat, scales="free_y")
+
+#=========================================
+
+#Ma et al. 2015. Daily temperature extremes play an important role in predicting thermal effects. The Journal of Experimental Biology 218 (14), 2289-2296
+#https://doi.org/10.1242/jeb.122127, no data in paper
+
+#=====
+#Zhao et al. The importance of timing of heat events for predicting the dynamics of aphid pest populations. Pest management science, 2019
+#https://doi.org/10.1002/ps.5344
+#survival and productivity
+#no data online
 
